d75a53abc69e40d87e8a0d6e8c0efdca09968ed8
elp2 <edwardpalmer@penguin.lxd>
2020-03-22 13:45:03 -0700

    Draw a clock.

---
+++ src/clock.cc
@@ -0,0 +1,50 @@
+#include "primitives/tuple.h"
+#include "display/canvas.h"
+#include "primitives/matrix.h"
+#include "primitives/transformation.h"
+#include "display/ppm_writer.h"
+
+#include <cassert>
+#include <cmath>
+#include <iostream>
+
+const float LINE_LENGTH = 20.0;
+const float LINE_INSET = 200.0;
+const int CANVAS_DIMENSION = 2 * LINE_INSET + 4 * LINE_LENGTH;
+const int INSET = CANVAS_DIMENSION / 2;
+
+void DrawPixel(Canvas *canvas, Tuple p) {
+  int x = floor(p.x()) + INSET;
+  int y = floor(p.y()) + INSET;
+  std::cout << x << ", " << y << std::endl;
+  canvas->WritePixel(Color(1, 0, 0), x, y);
+}
+
+int main(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
+
+  Canvas *canvas = new Canvas(CANVAS_DIMENSION, CANVAS_DIMENSION);
+
+  Tuple line_start = TupleFromPoint(0.0, LINE_INSET, 0.0);
+  Tuple line_end = TupleFromPoint(0.0, LINE_INSET + LINE_LENGTH, 0.0);
+
+  Matrix tick_rotation = RotationZ(2.0 * M_PI / 12.0);
+  for (int hour = 0; hour < 12; ++hour) {
+    Tuple line_slope = Scaling(1.0 / LINE_LENGTH, 1.0 / LINE_LENGTH, 1.0 / LINE_LENGTH) 
+      * (line_end - line_start);
+    Tuple point = line_start;
+    for (int pixel = 0; pixel < LINE_LENGTH; ++pixel) {
+      DrawPixel(canvas, point);
+      point = point + line_slope;
+    }
+
+    line_start = tick_rotation * line_start;
+    line_end = tick_rotation * line_end; 
+  }
+
+  PPMWriter ppm_writer = PPMWriter(canvas);
+  assert(ppm_writer.WriteFile("clock.ppm"));
+
+  return 0;
+}
