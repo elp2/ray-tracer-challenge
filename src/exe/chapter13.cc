e22916174b60bfc3688861485907b71b45127b20
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-04-17 20:22:37 -0700

    10x everything to make cone acne more obvious.

--- src/exe/chapter13.cc
+++ src/exe/chapter13.cc
@@ -26,22 +26,22 @@ const int CAMERA_DIMENSION = 100;
 
 Camera get_camera1() {
   Camera c = Camera(CAMERA_DIMENSION, CAMERA_DIMENSION, M_PI / 4.0);
-  Tuple from = Point(-16, 4.0, 0.0);
-  Tuple to = Point(0.0, 6.0, 3.0);
-  Tuple up = Vector(0.0, 1.0, 0.0);
+  Tuple from = Point(-160, 40.0, 0.0);
+  Tuple to = Point(0.0, 6.0, 30.0);
+  Tuple up = Vector(0.0, 10.0, 0.0);
   c.set_transform(ViewTransformation(from, to, up));
   return c;
 }
 
 World get_world1() {
   World w = World();
-  w.set_light(PointLight(Point(-8, 8.0, 0.0), Color(1.0, 1.0, 1.0)));
+  w.set_light(PointLight(Point(-80, 80, 0.0), Color(1.0, 1.0, 1.0)));
 
   auto p = new Plane();
   Material pm1;
   auto stripe1 = new StripedPattern(Color(0.5234, 0.3711, 0.2617),
       Color(0.6078, 0.4627, 0.3255));
-  stripe1->set_transform(Scaling(0.25, 0.6, 0.25) * RotationY(M_PI / 3));
+  stripe1->set_transform(Scaling(2.5, 6, 2.5) * RotationY(M_PI / 3));
   auto perturbed_plane = new PerturbedPattern(stripe1);
   pm1.set_pattern(perturbed_plane);
   pm1.set_reflective(0.2);
@@ -50,14 +50,15 @@ World get_world1() {
   w.add_object(p);
 
   Cylinder *blue_cylinder = new Cylinder();
+  blue_cylinder->SetTransform(Scaling(10, 10, 10));
   Material m = Material();
   m.set_color(ElectricBlueColor());
   m.set_reflective(0.3);
   blue_cylinder->set_material(m);
   w.add_object(blue_cylinder);
 
-  Cylinder *yellow_cylinder = new Cylinder(3, 1, true);
-  yellow_cylinder->SetTransform(Scaling(2, 2, 2) * Translation(0, 3, 3));
+  Cylinder *yellow_cylinder = new Cylinder(30, 0, true);
+  yellow_cylinder->SetTransform(Scaling(20, 20, 20) * Translation(-40, 0, 10));
   Material my = Material();
   my.set_color(YellowColor());
   my.set_reflective(0.3);
@@ -65,7 +66,7 @@ World get_world1() {
   w.add_object(yellow_cylinder);
 
   Cone *cone = new Cone();
-  cone->SetTransform(Translation(3, 3, 3));
+  cone->SetTransform(Translation(30, 30, 30));
   w.add_object(cone);
 
   return w;
