34ac354163bb217cbd3e986f5015e352574b8945
Edward Palmer <891364+elp2@users.noreply.github.com>
2020-05-30 14:27:32 -0700

    Render chessboard.

--- src/exe/chessboard.cc
+++ src/exe/chessboard.cc
@@ -31,18 +31,26 @@
 #include <fstream>
 #include <iostream>
 
-const int CAMERA_DIMENSION = 200;
+const int CAMERA_DIMENSION = 1000;
+const bool SUPER_SAMPLE = true;
+const float CHESSBOARD_WIDTH = 2.5;
+
+const Tuple light_position =  Point(3, 5, 5);
+const Color light_intensity = Color(1.0, 1, 1);
+// Light *LIGHT = new PointLight(light_position, light_intensity);
+Light *LIGHT = new SpotLight(light_position, light_intensity, 0.3, 0.5, Point(0, 0, 0));
+
 
 Camera get_camera1(bool supersample) {
   Camera c = Camera(CAMERA_DIMENSION, CAMERA_DIMENSION, M_PI / 4.0, supersample);
-  Tuple from = Point(6, 4, -1);
+  Tuple from = Point(12, 8, -2);
   Tuple to = Point(0, 0, 0);
   Tuple up = Vector(0, 1, 0);
   c.set_transform(ViewTransformation(from, to, up));
   return c;
 }
 
-Group *get_pawn() {
+Group *get_pawn(float x, float z) {
   std::ifstream model("chess.obj");
   assert(model.is_open());
   std::stringstream ss;
@@ -51,8 +59,8 @@ Group *get_pawn() {
 
   ObjParser *parser = new ObjParser(ss);
   auto g = parser->GroupNamed("PEDONE12")->OptimizedSubgroups(5, true, false, true);
-  Matrix scaling = Scaling(1.0 / 600.0, 1.0 / 600.0, 1.0 / 600.0);
-  g->SetTransform(scaling);
+  Bounds b = g->UnitBounds();
+  g->SetTransform(Translation(x * CHESSBOARD_WIDTH, fabs(b.minimum().y()), z * CHESSBOARD_WIDTH));
   return g;
 }
 
@@ -60,14 +68,14 @@ World get_world(Light *light) {
   World w = World();
   w.set_light(light);
 
-  auto s = new Sphere();
-  w.add_object(s);
-
-  auto *pawn = get_pawn();
-  w.add_object(pawn);
+  w.add_object(get_pawn(0, 0));
+  w.add_object(get_pawn(0, 1));
+  w.add_object(get_pawn(0, -1));
 
   auto *chessboard = new Plane();
   auto chessboard_pattern = new ThreeDPattern(WhiteColor(), BlackColor());
+  chessboard_pattern->set_transform(Scaling(CHESSBOARD_WIDTH, CHESSBOARD_WIDTH, CHESSBOARD_WIDTH)
+      * Translation(-0.5, -0.5, -0.5));
   auto chessboard_material = Material();
   chessboard_material.set_reflective(0.4);
   chessboard_material.set_pattern(chessboard_pattern);
@@ -78,8 +86,6 @@ World get_world(Light *light) {
 }
 
 void render_chessboard() {
-  Tuple position =  Point(3, 5, 5);
-  Color intensity = Color(1.0, 1.0, 1.0);
 
   // std::cout << "Rendering area_light.png." << std::endl;
   // auto canvas2 = get_camera2().Render(get_world(new AreaLight(position, intensity, 0.2, 2)));
@@ -87,7 +93,7 @@ void render_chessboard() {
   // ppm_writer2.WriteFile("area_light.png");
 
   std::cout << "Rendering chessboard.png." << std::endl;
-  auto canvas2 = get_camera1(false).Render(get_world(new PointLight(position, intensity)));
+  auto canvas2 = get_camera1(SUPER_SAMPLE).Render(get_world(LIGHT));
   PNGWriter ppm_writer2 = PNGWriter(canvas2);
   ppm_writer2.WriteFile("chessboard.png");
 }
